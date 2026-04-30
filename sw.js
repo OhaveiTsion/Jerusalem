db231fdb8f4001073173cf78ffa5d7e9069a9e2f
Greg <101050106+GregySite@users.noreply.github.com>
2026-04-30 21:20:46 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
- 
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Nécessaire pour que le bouton d'installation soit activé par le navigateur
+  return;
+});
